8008f78489b364826387ed50c12df01dead567c0
lanzay <lanzay@gmail.com>
2020-01-24 00:26:32 +0300

    test

--- main.go
+++ main.go
@@ -3,5 +3,9 @@ package main
 import "log"
 
 func main() {
+	Prn()
+}
+
+func Prn() {
 	log.Println("[D] Start")
 }
---
+++ prn_test.go
@@ -0,0 +1,7 @@
+package main
+
+import "testing"
+
+func TestPrn(t *testing.T) {
+	Prn()
+}
